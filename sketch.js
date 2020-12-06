4750c5a8b7b760e4f13d91d440a0690506a3905d
Prerna2008 <72653361+Prerna2008@users.noreply.github.com>
2020-12-06 16:48:42 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+
+var engine,world;
+var bob1,bob2,bob3,bob3,bob4,bob5;
+var rope1,rope2,rope3,rope4,rope5;
+var platform;
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+	engine = Engine.create();
+  world = engine.world;
+  
+
+  platform = new Ground(100,200, 200, 50);
+  bob1 = new Bob(300,500,25); 
+	bob2 = new Bob(350,500,25);
+	bob3 = new Bob(400,500,25);
+	bob4 = new Bob(450,500,25); 
+  bob5 = new Bob(500,500,25); 
+ 
+ 
+}
+
+function draw() {
+  background("green");
+  Engine.update(engine);
+  strokeWeight(4);
+  keyPressed();
+  platform.display();
+  bob1.display();
+  bob2.display();
+  bob3.display();
+  bob4.display();
+  bob5.display();
+}
+function keyPressed(){
+  if(keyCode===UP_ARROW){
+	Matter.Body.applyForce(paper.body,paper.body.position,{x:-4,y:3})
+}
+if(keyCode===DOWN_ARROW){
+	Matter.Body.applyForce(bob1.body,bob1.body.position,{x:0,y:0})
+}
+}
+
